c5ba3940fad14a2c3fd14dd7f37f0c186ef97590
Abir Mondal <102358238+abirmondal7864@users.noreply.github.com>
2025-07-18 17:44:33 +0530

    updated commit

---
+++ src/recursion/TOH.java
@@ -0,0 +1,17 @@
+package recursion;
+
+public class TOH {
+    public static void solveTOH(int n,String source,String  auxiliary,String destination){
+        if (n == 1) {
+            System.out.println("Move Disk 1 from "+source+" to "+destination);
+            return;//Imp***
+        }
+        solveTOH(n-1,source,destination,auxiliary);//Step 1
+        System.out.println("Move disk "+ n+" from "+ source+ " to "+destination);//Step 2
+        solveTOH(n-1,auxiliary,source,destination);//Step 3
+    }
+    public static void main(String[] args) {
+        int n=4;
+        solveTOH(n,"A","B","C");
+    }
+}
